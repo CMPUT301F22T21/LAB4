3673cef57325978dce7c56ce582fcb4cf7dc5dba
Shiming Zhang <shiming2@ualberta.ca>
2022-10-03 12:57:10 -0600

    update string color

--- app/src/main/java/com/example/lab4/Rhombus.java
+++ app/src/main/java/com/example/lab4/Rhombus.java
@@ -2,6 +2,7 @@ package com.example.lab4;
 
 public class Rhombus extends Shape{
     public static void main(String[] args) {
+        String color = "blue";
         System.out.println("this is Rhombus");
     }
 }
